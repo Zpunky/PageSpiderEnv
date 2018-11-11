566733670111e5d1145fa832110fd93e1432cfbb
ahab <christduncan@gmail.com>
2018-11-10 17:08:02 -0800

    PEP 8 corrections

--- page_spider.py
+++ page_spider.py
@@ -1,7 +1,8 @@
 import os
 import argparse
 
-#str is path to db file, url_string is list of URLs to crawl
+
+# str is path to db file, url_string is list of URLs to crawl
 def main(database: str, url_list_file: str):
     print("we are going to work with " + database)
     print("we are going to scan " + url_list_file)
